57573c4261db1b12f388da3de633b1b01c64755e
Jessica de Marchi <37842830+jessicademarchi@users.noreply.github.com>
2020-08-05 19:43:24 -0300

    aula 1
    faltam ajustes

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/37842830?s=460&u=f446c1c5e4690d3bfffa2b01080b65122806a81e&v=4" alt="Jessica de Marchi" />
+                <div>
+                    <strong>Jessica de Marchi</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br /><br />
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="WhatsApp" />
+                        Entrar em contato
+                    </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -12,7 +13,7 @@ import './style.css';
 function Landing() {
     return (
         <div id="page-landing">
-            <div id="page-lading-content" className="container">
+            <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy" />
                     <h2>Sua plataforma de estudos online.</h2>
@@ -25,14 +26,14 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar" />
-                        Estudar
-                    </a>
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
+                        <img src={giveClassesIcon} alt="Dar aulas" />
+                        Dar aulas
+                    </Link>
                 </div>
 
                 <span className="total-connections">
